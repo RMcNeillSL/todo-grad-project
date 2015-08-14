7b4a631190cce671d9d7b5ceeb1af5bfb4ca08e2
Robert McNeill <rmcneill@scottlogic.com>
2015-08-14 13:22:03 +0100

    Temporary config changes

--- test/e2eHelpers.js
+++ test/e2eHelpers.js
@@ -23,9 +23,13 @@ module.exports.setupDriver = function() {
 module.exports.setupServer = function(done) {
     router = express.Router();
     if (gatheringCoverage) {
-        router.get("/main.js", function(req, res) {
-            var absPath = path.join(__dirname, "..", "public", "main.js");
-            res.send(instrumenter.instrumentSync(fs.readFileSync("public/main.js", "utf8"), absPath));
+        router.get("/app/app.js", function(req, res) {
+            var absPath = path.join(__dirname, "..", "public", req.path);
+            res.send(instrumenter.instrumentSync(fs.readFileSync("public/" + req.path, "utf8"), absPath));
+        });
+        router.get("/app/todoList", function(req, res) {
+            var absPath = path.join(__dirname, "..", "public", req.path);
+            res.send(instrumenter.instrumentSync(fs.readFileSync("public/" + req.path, "utf8"), absPath));
         });
     }
     server = createServer(testPort, router, done);
