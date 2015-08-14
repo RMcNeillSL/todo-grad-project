18944a089e49969fb6d3cc3653eb6caf8263bb03
Robert McNeill <rmcneill@scottlogic.com>
2015-08-14 12:07:49 +0100

    Started fixing grunt errors, removed files no longer needed

--- public/app/app.js
+++ public/app/app.js
@@ -1,12 +1,11 @@
-'use strict';
+"use strict";
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+
+angular.module("myApp", [
+  "ngRoute",
+  "myApp.todoList"
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/todoList'});
+config(["$routeProvider", function($routeProvider) {
+    $routeProvider.otherwise({redirectTo: "/todoList"});
 }]);
--- public/app/todoList/todoList.js
+++ public/app/todoList/todoList.js
@@ -1,15 +1,15 @@
-'use strict';
+"use strict";
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module("myApp.todoList", ["ngRoute"])
 
-    .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/todoList', {
-            templateUrl: 'todoList/todoList.html',
-            controller: 'todoListCtrl'
+    .config(["$routeProvider", function ($routeProvider) {
+        $routeProvider.when("/todoList", {
+            templateUrl: "todoList/todoList.html",
+            controller: "todoListCtrl"
         });
     }])
 
-    .controller('todoListCtrl', ["$scope", "$http", function ($scope, $http) {
+    .controller("todoListCtrl", ["$scope", "$http", function ($scope, $http) {
         $scope.todos = [];
         $scope.newTodo = "";
         $scope.filterFunction = function () { return true; };
@@ -29,13 +29,13 @@ angular.module('myApp.view1', ['ngRoute'])
                     else {
                         $scope.incompleteCount++;
                     }
-                })
+                });
 
             }, function (response) {
-                error.textContent = "Failed to get list. Server returned " +
+                $scope.errorText = "Failed to get list. Server returned " +
                     response.status + " - " + response.statusText;
             });
-        }
+        };
 
         $scope.createTodo = function () {
             $http.post("/api/todo", {
@@ -44,37 +44,37 @@ angular.module('myApp.view1', ['ngRoute'])
                 $scope.newTodo = "";
                 $scope.getTodoList();
             }, function (response) {
-                error.textContent = "Failed to create item. Server returned " + response.status +
+                $scope.errorText = "Failed to create item. Server returned " + response.status +
                 " - " + response.statusText;
-             })
-        }
+            });
+        };
 
         $scope.deleteTodo = function (todo) {
             $http.delete("/api/todo/" + todo.id).then(function (response) {
                 $scope.getTodoList();
             }, function (response) {
-                error.textContent = "Failed to delete todo list item with ID " +
+                $scope.errorText = "Failed to delete todo list item with ID " +
                 todo.id + ". Server returned " + response.status + " - " + response.statusText;
-            })
-        }
+            });
+        };
 
         $scope.completeTodo = function (todo) {
             $http.put("/api/todo/" + todo.id).then(function (response) {
                 $scope.getTodoList();
             }, function (response) {
-                error.textContent = "Failed to complete todo list item with ID " +
+                $scope.errorText = "Failed to complete todo list item with ID " +
                 todo.id + ". Reason: " + response.status + " - " + response.responseText;
-            })
-        }
+            });
+        };
 
         $scope.deleteComplete = function () {
             $http.delete("/api/todo").then(function (response) {
                 $scope.getTodoList();
             }, function (response) {
-                error.textContent = "Failed to delete completed todo list items. Reason: " +
+                $scope.errorText = "Failed to delete completed todo list items. Reason: " +
                 this.status + " - " + this.responseText;
-            })
-        }
+            });
+        };
 
         $scope.applyFilter = function (filter) {
             if (filter === "complete") {
@@ -87,7 +87,7 @@ angular.module('myApp.view1', ['ngRoute'])
                 $scope.filterFunction = function () { return true; };
             }
             $scope.getTodoList();
-        }
+        };
 
         $scope.getTodoList();
     }]);
